0aebf97af9c5949702aa582eb3775579bdaaaa60
poojas17 <poojas.mcs.du.2015@gmail.com>
2023-03-08 19:24:47 +0530

    first commit

---
+++ DSAlgoPractice/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgoPractice
+{
+    public class Calculator
+    {
+        public Calculator() { }
+        public int sum(int x, int y) { return x + y; }
+
+
+        public static void Main()
+        {
+            Calculator calculator = new Calculator();
+            calculator.sum(1, 2);
+        }
+    }
+}
---
+++ DSAlgoUnitTests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using DSAlgoPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgoUnitTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Calculator_AddTest()
+        {
+            Calculator calculator = new Calculator();
+            int sum = calculator.sum(1, 2);
+            Assert.AreEqual(3, sum,0,"Sum incorrect");
+        }
+    }
+}
